9467764ef10dd51eb26fc604979c29f98f4ea9fe
Igor Pavloski <Igor.Pavloski@seavus.com>
2018-10-11 12:58:54 +0200

    added redux

--- hacker-news-clone/src/index.js
+++ hacker-news-clone/src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import App from 'components/App/App';
 import setGlobalStyles from 'styles/globals';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
 
 setGlobalStyles();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = () => {
+    const initialState = {};
+    const store = configureStore(initialState);
+
+    store.dispatch({ type: '@hnClone/@@INIT' });
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root'));
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
